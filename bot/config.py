da642c0c5cd6b7f830f56b7aa238db8a4d0c720b
Cesar Rodriguez <cesar@ooyala.com>
2015-07-01 13:55:44 -0500

    Add environment variables

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,9 @@ import logging
 import pytz
 
 BOT_URL = os.getenv("BOT_URL", "")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+WEBHOOK = os.getenv("WEBHOOK", "")
+WEBHOOK_PORT = os.getenv("WEBHOOK_PORT", "8080")
 LAST_UPDATE_ID_FILE = "last_update"
 TAGS_FILE = "tags"
 POLL_PERIOD = 1
