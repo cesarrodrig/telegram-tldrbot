135bfa6637523667bb1151233c6f74ad141e4779
Cesar Rodriguez <cesar@ooyala.com>
2015-07-09 16:57:39 -0500

    Add users persistence

--- bot/mapper.py
+++ bot/mapper.py
@@ -1,5 +1,7 @@
 import jsonpickle
 
+from config import *
+
 class Mapper(object):
 
     def __init__(self, db_name):
@@ -17,11 +19,28 @@ class TextFileMapper(Mapper):
         except:
             pass
 
+        self.users = {}
+        try:
+            f = open(USERS_COLLECTION_NAME)
+            self.users = jsonpickle.decode(f.read())
+            f.close()
+        except:
+            pass
+
     def get_chat_by_id(self, id):
         return self.chats.get(id)
+
+    def get_user_by_id(self, id):
+        return self.users.get(id)
 
     def save_chat(self, chat):
         self.chats[chat.id] = chat
         f = open(self.db_name, "w")
         f.write(jsonpickle.encode(self.chats))
+        f.close()
+
+    def save_user(self, user):
+        self.users[user.id] = user
+        f = open(USERS_COLLECTION_NAME)
+        f.write(jsonpickle.encode(self.users))
         f.close()
